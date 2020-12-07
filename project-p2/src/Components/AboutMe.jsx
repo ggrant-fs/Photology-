e06ab0b3d96eba60f5211f45d2a5f95fc76822aa
Gary Grant <garygrant@Garys-MacBook-Air.local>
2020-12-07 14:45:39 -0500

    section 1 about me page completed

--- project-p2/src/Components/AboutMe.jsx
+++ project-p2/src/Components/AboutMe.jsx
@@ -5,7 +5,19 @@ const AboutMe = () => {
     return (
         <div >
             <AboutHeader/>
-            <div className="pic-test"></div>  
+            <div className="about-container">
+                <div className="content">
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </div>
+                <div className="break"></div>
+                <div id="pic-div">
+                   <img id="pic-test" src="https://i.imgur.com/5i93lwO.jpg"/>
+                </div>
+                <div className="break"></div>
+                
+            </div>
         </div>
     );
 };
