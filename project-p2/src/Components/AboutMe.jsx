c8be0a423af55a3ad13c30e24d00feb7b5cec241
Gary Grant <garygrant@garys-air.myfiosgateway.com>
2020-12-08 01:01:04 -0500

    updated files

--- project-p2/src/Components/AboutMe.jsx
+++ project-p2/src/Components/AboutMe.jsx
@@ -1,23 +1,13 @@
 import React from 'react';
 import AboutHeader from './AboutHeader'
+import SecOneAboutMe from './SecOneAboutMe'
+// import SecTwoAboutMe from './SecTwoAboutMe'
 
 const AboutMe = () => {
     return (
         <div >
             <AboutHeader/>
-            <div className="about-container">
-                <div className="content">
-                    <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                </div>
-                <div className="break"></div>
-                <div id="pic-div">
-                   <img id="pic-test" src="https://i.imgur.com/5i93lwO.jpg"/>
-                </div>
-                <div className="break"></div>
-                
-            </div>
+            <SecOneAboutMe/>
         </div>
     );
 };
--- project-p2/src/Components/ShowPage.jsx
+++ project-p2/src/Components/ShowPage.jsx
@@ -4,6 +4,20 @@ const ShowPage = () => {
     return (
         <div>
           <ShowHeader/>
+          <div className="show-photos">
+            <div id="show-pic1">
+               <img src="https://i.imgur.com/OA7BRCB.jpg"/>
+            </div>
+            <div id="show-pic2">
+               <img src="https://i.imgur.com/pAT1mbV.jpg"/>
+            </div>
+            <div id="show-pic3">
+               <img src="https://i.imgur.com/K7zUaeb.jpg"/>
+            </div>
+            <div id="show-pic4">
+               <img src="https://i.imgur.com/woCmidR.jpg"/>
+            </div>
+          </div>
         </div>
     );
 };
