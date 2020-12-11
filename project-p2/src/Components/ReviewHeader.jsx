408d3dcd5db471d8d702844c1ea630db32b23b98
Gary Grant <garygrant@Garys-MacBook-Air.local>
2020-12-11 07:56:07 -0500

    saved reviews

--- project-p2/src/Components/ReviewHeader.jsx
+++ project-p2/src/Components/ReviewHeader.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 const ReviewHeader = () => {
     return (
         <div>
-            <header id="review-header" className="header-template"></header>
+            <header id="review-header" className="header-template">
+            <h1 id="review-page-title" className="homepage-h1">Photology</h1>
+            <h1 id="review-header-title" className="homepage-h1">Reviews</h1>
+            
+            </header>
         </div>
     );
 };
